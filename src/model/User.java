bda4c820d2262c6e307984489af84b09eadd3d11
Likhithanjali-2 <liki2444@gmail.com>
2020-03-26 12:11:13 -0500

    initial commit-

--- src/model/User.java
+++ src/model/User.java
@@ -1,3 +1,14 @@
 package model;
-
-//Type your code here
+import java.time.LocalDate;
+public class User{
+	String name;
+	String email;
+	public String dob;
+	String gender;
+	public User(String name,String email,String dob,String gender){
+		this.name=name;
+		this.email=email;
+		this.dob=dob;
+		this.gender=gender;
+	}
+}
